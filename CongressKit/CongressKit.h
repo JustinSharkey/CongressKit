ae397aa88eb75e72758b3bd1eb762537fa83f895
Justin Sharkey <justin.sharkey@me.com>
2018-03-10 18:20:40 -0500

    Initial Commit

---
+++ CongressKit/CongressKit.h
@@ -0,0 +1,19 @@
+//
+//  CongressKit.h
+//  CongressKit
+//
+//  Created by Justin Sharkey on 3/10/18.
+//  Copyright © 2018 Justin Sharkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CongressKit.
+FOUNDATION_EXPORT double CongressKitVersionNumber;
+
+//! Project version string for CongressKit.
+FOUNDATION_EXPORT const unsigned char CongressKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CongressKit/PublicHeader.h>
+
+
